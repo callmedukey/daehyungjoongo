6f0da09c7d31051f7f1675e062f7a2ffca1c809d
root <root@duke>
2024-09-25 13:18:49 +0900

    fuuuu

---
+++ declarations.d.ts
@@ -0,0 +1,14 @@
+declare interface Car {
+    car: string;
+    year: string;
+    buy: string;
+    export: string;
+    markup: string;
+    src: StaticImageData;
+}
+
+declare interface Stages {
+    src: StaticImageData;
+    title: string;
+}
+declare module '@splidejs/react-splide';
